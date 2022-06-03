8818b877c95b5ddf196624f9e58b5face74b6883
Mudit Choudhary <cmudit60@gmail.com>
2022-06-03 11:23:35 +0530

    Initial setup

---
+++ script.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+//   Take user input for the book
+// Store the book object into the array
+
+}
+
+function displaybook() {
+    // Loop through each book
+    // Display books in a formatted way
+}
+
+// Button to add new books
+// Button on book display to remove a book
+    // Need to assiociate actual book with dom with data atribute
+// Button on book display to change book read status
+    // function to change that book object read status
